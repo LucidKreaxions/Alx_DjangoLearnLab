e51ebf51d9c06d5c8cf6c33472c01cf21d0ac9b9
Steve Chege <chegekstephen@gmail.com>
2025-02-23 20:24:24 -0800

    Fixed query for checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,7 +10,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)
     return [book.title for book in books]
 
 # List all books in a library

8df4203d101cd2b82afc47873eb883badd8104f7
Steve Chege <chegekstephen@gmail.com>
2025-02-25 07:24:31 -0800

    Fixed missing template_name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path  # ✅ Import path
-from django.contrib.auth import views as auth_views # Import Django's auth views
+from django.contrib.auth.views import LoginView, LogoutView 
 from .views import list_books, LibraryDetailView, register  # ✅ Import both views
 
 urlpatterns = [
@@ -8,8 +8,8 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # ✅ Class-based view
 
     # Authentication URLs
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name+'relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'), # Custom Registration view
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'), # Login view
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'), # Logout view
 ]
 

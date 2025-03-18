e37ca4bd7e9f2c0685c5ce169cbe7ca7bc54a7c1
Steve Chege <chegekstephen@gmail.com>
2025-03-17 21:46:23 -0700

    Added BookViewSet for full CRUD API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,20 @@
+from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
 
 class BookList(ListAPIView):
+    """
+    API View that lists all books (Read-only).
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet that provides CRUD operations for the Book model.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer

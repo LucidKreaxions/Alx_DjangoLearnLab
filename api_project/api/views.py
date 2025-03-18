406116d697762e8fe7e91a86d5895d92b582e8f5
Steve Chege <chegekstephen@gmail.com>
2025-03-17 22:17:58 -0700

    Added authentication & permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
@@ -15,6 +16,8 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     """
     ViewSet that provides CRUD operations for the Book model.
+    Rewuires authentication for access.
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
